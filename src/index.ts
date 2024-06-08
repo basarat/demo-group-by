6822676d23b17eb3425c726e76fd56790ca25e5f
basarat <basaratali@gmail.com>
2024-06-08 18:34:23 +1000

    wrap

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-async function main() {
-  
-}
+const inventory = [
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
 
-main();
+const traditional = inventory.reduce((acc, current) => {
+  if (acc[current.type]) acc[current.type].push(current);
+  else acc[current.type] = [current];
+  return acc;
+}, Object.create(null));
+
+const result = Object.groupBy(
+  inventory,
+  (item) => item.type
+);
+
+console.log(result);
